1a5ac93c6f46c24ea675c2d955653c257154c277
Joao <joao.lousada@crosslend.com>
2021-05-01 12:08:24 +0200

    css-modules

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './styles.module.css';
 
-export default function Article({title, description}) {
+export default function Article() {
   return (
-    <div className="article">
-      <h2 className="article__title">Titulo do artigo</h2>
-      <p className="article__description">description do artigo...</p>
+    <div className={styles.wrapper}>
+      <h2 className={styles.header}>Titulo do artigo</h2>
+      <p className={styles.content}>description do artigo...</p>
     </div>
   )
 }
--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import styles from './styles.module.css';
 
 export default function Card() {
   return (
-    <div class="card">
-      <h2 className="title">Titulo do card</h2>
-      <p className="description">descricao do card</p>
+    <div class={styles.wrapper}>
+      <h2 className={styles.header}>Titulo do card</h2>
+      <p className={styles.content}>descricao do card</p>
     </div>
   )
 }
